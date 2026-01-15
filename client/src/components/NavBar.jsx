e9fd66185e38d3a1786c9082bef19cfdf0387f7a
Nara Ratanakkithya <narakithya@gmail.com>
2026-01-15 18:23:47 +0700

    added Navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const logoutUser = () => {
+    navigate("/");
+  };
+  const user = {
+    name: "jordan",
+    email: "jordan.lee@example.com",
+  };
+  return (
+    <div className="shadow bg-white">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all">
+        <Link to={"/"}>
+          <img src="/logo.svg" className="h-11 w-auto" />
+        </Link>
+      </nav>
+      <div className="flex items-center gap-4 text-sm">
+        <p className="max-sm:hidden">Hi {user?.name}</p>
+        <button
+          onClick={logoutUser}
+          className="bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
